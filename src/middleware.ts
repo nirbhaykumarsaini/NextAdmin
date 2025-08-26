a026e004f111a8268a10312868a15debe0d9fbf8
Nirbhay Kumar Saini <140544127+nirbhaykumarsaini@users.noreply.github.com>
2025-08-26 15:49:41 +0530

    error

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   // Set CORS headers
   response.headers.set('Access-Control-Allow-Origin', '*'); // Or specific origins
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCHS, OPTIONS');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   return response;
 }
