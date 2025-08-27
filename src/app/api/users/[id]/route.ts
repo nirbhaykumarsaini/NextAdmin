3fa6151318f41aedae161322ce02ccf521f1bff4
Nirbhay Kumar Saini <140544127+nirbhaykumarsaini@users.noreply.github.com>
2025-08-27 18:32:03 +0530

    api error

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -68,7 +68,7 @@ export async function PUT(request: NextRequest,{ params }: { params: Promise<{ i
   }
 }
 
-export async function DELETE({ params }: { params: Promise<{ id: string }> }) {
+export async function DELETE(request: NextRequest,{ params }: { params: Promise<{ id: string }> }) {
   try {
 
     const {id} = await params;
