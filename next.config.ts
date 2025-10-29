628c2f98810bdbce971c79faec3b41376ef6e5b6
Nirbhay Kumar Saini <140544127+nirbhaykumarsaini@users.noreply.github.com>
2025-10-29 16:31:03 +0530

    any error

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
+const nextConfig:NextConfig = {
   env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION,
+    JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
   },
+  // If you're using static exports
+  output: 'standalone', // or 'export' depending on your setup
 }
 
 module.exports = nextConfig
-
-export default nextConfig;
