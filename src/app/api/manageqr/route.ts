4e41cef9fc0652832bd205f521dff7c78aecab5e
Nirbhay Kumar Saini <140544127+nirbhaykumarsaini@users.noreply.github.com>
2025-11-07 13:42:02 +0530

    cloudnary image added

--- src/app/api/manageqr/route.ts
+++ src/app/api/manageqr/route.ts
@@ -6,9 +6,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // 🧠 Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
-  api_key: process.env.CLOUDINARY_API_KEY!,
-  api_secret: process.env.CLOUDINARY_API_SECRET!,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 interface UpdateData {
